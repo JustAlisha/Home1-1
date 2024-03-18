ef63891ce975c26ecb14e0f5b38346a18b4e54dd
Test <bloodreighn965@gmail.com>
2024-03-18 22:27:56 +0300

    гзвUpdate

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+class main {
+    public static void main(String[] args) {
+
+        int initialBalance = 1300;
+        int addend = 500;
+
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
+        int finalBalance = initialBalance + addend + bonus;
+        System.out.println("Итоговый счет:" + finalBalance);
+        System.out.println("Бонус:" + bonus);
+
+    }
 }
